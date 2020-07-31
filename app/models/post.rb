dda23ce383ab91d0a12c38cc1125673f10ab4f97
steven <steven@tenlong.com.tw>
2020-07-31 14:07:35 +0800

    PPT

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :board
   validates :title, presence: true,  :length => {:minimum => 2, :maximum => 10}
-
+  validates :serial, uniqueness: true
   
+  before_save :create_serial
+
+  private
+  def create_serial
+    self.serial = serial_generator(10)
+  end
+
+  def serial_generator(n)
+    [*'a'..'z', *'A'..'Z', *0..9].sample(n).join
+  end
+
 
 end
