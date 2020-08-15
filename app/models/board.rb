ac15efb2f117a7b1161a9852a1945d7d7b7ee9a2
steven <steven@tenlong.com.tw>
2020-08-15 13:02:53 +0800

    PPT

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,7 +4,6 @@ class Board < ApplicationRecord
     has_many :posts
     has_many :board_masters
     has_many :users, through: :board_masters
-
     has_many :favorite_boards
     has_many :favorited_users, through: :favorite_boards ,source: :user
 
